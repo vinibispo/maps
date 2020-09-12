9fafa271c0f77d18ff69fea9720a6381431529d0
vinibispo <vini.bispo015@gmail.com>
2020-09-12 20:34:47 -0300

    Creating service for API

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,13 @@
 module.exports = {
+  arrowParens: 'always',
   bracketSpacing: false,
+  embeddedLanguageFormating: 'auto',
+  jsxSingleQuote: true,
+  printWidth: 60,
   jsxBracketSameLine: true,
+  semi: false,
+  proseWrap: 'preserve',
   singleQuote: true,
-  trailingComma: 'all',
-};
+  trailingComma: 'es5',
+  useTabs: false,
+}
--- App.tsx
+++ App.tsx
@@ -1,27 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import MapboxGL from "@react-native-mapbox-gl/maps"
-
-MapboxGL.setAccessToken('pk.eyJ1IjoiZ3VzdGF2b2RhY3J2aSIsImEiOiJja2VzaGgzcTAwMXE2MnVvM2RvYnN6OXl6In0.d7iDzCPH5H7dlokHQO4sGA')
-
-import React from 'react';
+import React from 'react'
+import MapboxGL from '@react-native-mapbox-gl/maps'
 import {
   SafeAreaView,
   StyleSheet,
   View,
-  StatusBar,
-} from 'react-native';
+  StatusBar
+} from 'react-native'
 
-const App = () => {
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiZ3VzdGF2b2RhY3J2aSIsImEiOiJja2VzaGgzcTAwMXE2MnVvM2RvYnN6OXl6In0.d7iDzCPH5H7dlokHQO4sGA'
+)
+
+const App: React.FC = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle='dark-content' />
       <SafeAreaView>
         <View style={styles.page}>
           <View style={styles.container}>
@@ -30,24 +23,24 @@ const App = () => {
         </View>
       </SafeAreaView>
     </>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
   container: {
     height: 800,
     width: 300,
-    backgroundColor: "tomato"
+    backgroundColor: 'tomato'
   },
   map: {
     flex: 1
   }
-});
+})
 
-export default App;
+export default App
